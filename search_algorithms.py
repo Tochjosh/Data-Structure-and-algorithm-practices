b2a5b067c100adedce874c0cfecf3c4ff6494436
Tochjosh <tochjosh@gmail.com>
2021-12-01 23:35:55 +0100

    added logic for rating movie

--- search_algorithms.py
+++ search_algorithms.py
@@ -72,12 +72,12 @@ def word_count(list_of_words, word):
     word_counter = {}
     for i in list_of_words:
         if i == word:
-            word_counter[word] = word_counter.get(word, 0) + 1 # uses the get method of a dict
+            word_counter[word] = word_counter.get(word, 0) + 1  # uses the get method of a dict
 
 
 def check_prime(num_lis):
     for num in num_lis:
-        for n in range(2, int(num/2)):  # exclude 1 and the number and check other number in-between
+        for n in range(2, int(num / 2)):  # exclude 1 and the number and check other number in-between
             if num % n == 0:
                 print("{} is not a prime number because {} is a factor of {}.".format(num, n, num))
                 break
@@ -92,3 +92,34 @@ def is_prime(num):
             return False
 
     return True
+
+
+# the code below is for rating movies and storing the movies in an array
+
+def star(message):
+    stars_val = input(message)
+    if int(stars_val) < 1 or int(stars_val) > 5:
+        print('Enter a value between 1 and 5')
+        star(message)
+
+    return int(stars_val)
+
+
+rated_movie = []
+
+
+def rate():
+    movie = input('enter a movie name: ').lower()
+    if movie != 'done':
+        rating = star('How many stars (1-5): ')
+        movie_arr = ['', '']
+        stars = '*' * rating
+        movie_arr[0] = movie
+        movie_arr[1] = stars
+        rated_movie.append(movie_arr)
+        rate()
+    if movie == 'done':
+        print(rated_movie)
+
+
+rate()
